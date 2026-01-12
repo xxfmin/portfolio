7617423eb1c5be09060e08d8faa990d2be7dcba7
Felipe Min <felipemin03@gmail.com>
2026-01-12 13:00:56 -0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { SmoothScroll } from "@/components/ui/SmoothScroll";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </SmoothScroll>
+        <Analytics />
       </body>
     </html>
   );
